d7b1e78465e2f1e387dab4a40b7c0206e089c208
Samuel Bachmann <samuel.bachmann@gmail.com>
2016-05-24 10:16:44 +0200

    move include/boost to include/roscpp_nodewrap/boost, remove cmake_modules
    * include/boost was not copied to /opt/ros/indigo/include/boost, which would be bad practice anyways. Therefore, include/boost was moved to include/roscpp_nodewrap/boost.
    
    * cmake_modules is not needed and causes build errors

---
+++ roscpp_nodewrap/include/roscpp_nodewrap/boost/accumulators/statistics.hpp
@@ -0,0 +1,61 @@
+///////////////////////////////////////////////////////////////////////////////
+/// \file statistics.hpp
+/// Includes all of the Statistical Accumulators Library
+//
+//  Copyright 2005 Eric Niebler. Distributed under the Boost
+//  Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef BOOST_ACCUMULATORS_STATISTICS_HPP_EAN_01_17_2006
+#define BOOST_ACCUMULATORS_STATISTICS_HPP_EAN_01_17_2006
+
+#include <roscpp_nodewrap/boost/accumulators/statistics/count.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/covariance.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/density.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/error_of.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/error_of_mean.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/extended_p_square.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/extended_p_square_quantile.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/kurtosis.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/max.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/mean.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/median.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/min.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/moment.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/peaks_over_threshold.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/pot_tail_mean.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/pot_quantile.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/p_square_cumul_dist.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/p_square_quantile.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/skewness.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/stats.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/sum.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/sum_kahan.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/tail.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/tail_quantile.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/tail_mean.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/tail_variate.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/tail_variate_means.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/variance.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/weighted_covariance.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/weighted_density.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/weighted_kurtosis.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/weighted_extended_p_square.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/weighted_mean.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/weighted_median.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/weighted_moment.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/weighted_peaks_over_threshold.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/weighted_p_square_cumul_dist.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/weighted_p_square_quantile.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/weighted_skewness.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/weighted_sum.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/weighted_sum_kahan.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/weighted_tail_quantile.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/weighted_tail_mean.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/weighted_tail_variate_means.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/weighted_variance.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/with_error.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/parameters/quantile_probability.hpp>
+#include <roscpp_nodewrap/boost/accumulators/statistics/variates/covariate.hpp>
+
+#endif
